09be6ee0d29ffe032f83dd806cc7b96a3b1db45d
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2024-01-18 19:36:08 +0100

    Practica 1a ejercicios 4 al 7

--- Practicas/Practica1a_Intr_Tratam_Imag/Practica_1a.m
+++ Practicas/Practica1a_Intr_Tratam_Imag/Practica_1a.m
@@ -22,15 +22,59 @@ disp("Mayor elemento: " + mayorElementoImagen1);
 %------------------------------------------------------
 % Ejercicio 5
 %------------------------------------------------------
-disp(Imagen1(338,1,1));
+%disp(Imagen1(338,1,1));
 [filas, columnas, dimensiones] = size(Imagen1);
-for fila = 1:filas
-  for columna = 1:columnas
-    for dimension = 1:dimensiones
-      disp("Imagen1[" + fila + ", " + columna ...
-          + ", " + dimension + "] = " + ...
-          Imagen1(fila, columna, dimension));
-    end
-  end
-end
+%Imagen2 = zeros(filas, columnas, dimensiones);
+
+%for fila = 1:filas
+%  for columna = 1:columnas
+%    for dimension = 1:dimensiones
+      %disp("Imagen1[" + fila + ", " + columna ...
+      %    + ", " + dimension + "] = " + ...
+      %    Imagen1(fila, columna, dimension));
+%      Imagen2(fila, columna, dimension) = 255 ...
+%        - Imagen1(fila, columna, dimension);
+
+%      disp("Imagen2[" + fila + ", " + columna ...
+%          + ", " + dimension + "] = " + ...
+%          Imagen2(fila, columna, dimension));
+%    end
+%  end
+%end
+
+Imagen2 = 255 - Imagen1;
+
+%imshow(Imagen2);
+
+%Se observa el mismo resultado que con imcomplement
+%imshow(imcomplement(Imagen1));
+
+%------------------------------------------------------
+% Ejercicio 6
+%------------------------------------------------------
+% La imagen es RGB, cada dimension representa uno de
+% esos canales, por lo cual los niveles de rojo estan
+% en el canal 1 (Red), siendo los canales 2 y 3 Green
+% y Blue respectivamente
+Imagen3 = Imagen1(:, :, 1);
+%imshow(Imagen3);
+%imhist(Imagen3);
+
+Imagen4 = imadjust(Imagen3, [], [], 0.5);
+%imshow(Imagen4);
+%imhist(Imagen4);
+
+Imagen5 = imadjust(Imagen3, [], [], 1.5);
+%imshow(Imagen5);
+%imhist(Imagen5);
+%------------------------------------------------------
+% Ejercicio 7
+%------------------------------------------------------
+Imagen6 = imabsdiff(Imagen4, Imagen5);
+%imshow(Imagen6);
+
+% La diferencia absoluta no es más que la resta de las
+% matrices de ambas imágenes
+Imagen6Manual = Imagen4 - Imagen5;
+imshow(Imagen6Manual);
 %------------------------------------------------------
