1ea6826fec286846ccb81bacaf43f7ae1c019f87
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2024-02-02 02:17:09 +0100

    Practica 1c - Ejercicio 1

--- Practicas/Practica1c_ProcesamBasicoImagenes/Practica_1c.m
+++ Practicas/Practica1c_ProcesamBasicoImagenes/Practica_1c.m
@@ -10,7 +10,7 @@ imagenOriginal = imread("P1b.jpg");
 
 mascaraBinaria = roipoly(imagenOriginal);
 
-figure, imshow(mascaraBinaria);
+%figure, imshow(mascaraBinaria);
 
 [filas, columnas] = size(mascaraBinaria);
 
@@ -25,7 +25,7 @@ for fila=1:filas
     end
 end
 
-figure, imshow(imagenRecortada);
+%figure, imshow(imagenRecortada);
 
 % Mediante multiplicacion de vectores
 %imagenRecortada2 = imagenOriginal.*uint8(mascaraBinaria);
@@ -42,5 +42,46 @@ maxColumna = max(columnasAreaInteres);
 
 areaInteres = imagenRecortada(minFila:maxFila, minColumna:maxColumna, :);
 
-figure, imshow(areaInteres);
+%figure, imshow(areaInteres);
+
+% Siendo N la altura de la imagen y M la anchura
+[N, M] = size(areaInteres);
+
+disp("anchura: " + M + ",altura: " + N);
+
+% Ejercicio 1
+% -------------------------------------------------------------------------
+sumatorioBrillo = uint64(0);
+
+for fila=1:N
+    for columna=1:M
+        sumatorioBrillo = sumatorioBrillo + ...
+        uint64(areaInteres(fila, columna));
+        %disp("areaInteres(" + fila + ", " + columna + "): " ...
+            %+ areaInteres(fila, columna));
+    end
+end
+
+brillo = sumatorioBrillo / (M * N);
+
+disp("Brillo: " + brillo);
+
+sumatorioContraste = uint64(0);
+
+for fila=1:N
+    for columna=1:M
+        sumatorioContraste = sumatorioContraste + ...
+        power(uint64(areaInteres(fila, columna)) - brillo, 2);
+    end
+end
+
+pasoIntermedioContraste = sumatorioContraste / (M * N);
+contraste = sqrt(double(pasoIntermedioContraste));
+
+disp("Contraste: " + contraste);
+
+% Ejercicio 2
+% -------------------------------------------------------------------------
+
+
 
